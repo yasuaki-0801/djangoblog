ea404e8ff1144b18e421452e0426c8345c1372c9
康明 浅野 <yasuaki199181@outlook.com>
2020-05-23 12:46:23 +0900

    AWSストレージ追加

---
+++ website/settings/local.py
@@ -0,0 +1,13 @@
+# settings/local.py
+import os
+from .base import *
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+ALLOWED_HOSTS = ['127.0.0.1']
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')  # 環境変数を指定
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')  # 環境変数を指定
+
+
